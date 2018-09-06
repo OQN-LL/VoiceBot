2110fd0449acbd0cc8da216889efa07ab61c50d0
OQN-LL <luta00000@gmail.com>
2018-09-07 01:48:11 +0900

    test2

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 import discord
+from discord.ext import commands
 
 with open("accessToken.txt","r") as f:
     token = f.read()[:-1]
 
-bot = discord.Client()
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('/'))
 
 @bot.event
 async def on_ready():
@@ -11,6 +12,25 @@ async def on_ready():
     print("login:",bot.user.name)
     print("ID:",bot.user.id)
     print("-"*40)
+
+@commands.command(pass_context=True, no_pm=True)
+async def summon(ctx):
+    summoned_channel = ctx.message.author.voice_channel
+    if summoned_channel is None:
+        await bot.say('You are not in a voice channel.')
+        return False
+
+    state = get_voice_state(ctx.message.server)
+    if state.voice is None:
+        state.voice = await bot.join_voice_channel(summoned_channel)
+    else:
+        await state.voice.move_to(summoned_channel)
+    return True
+
+#@bot.event
+#async def on_message(message):
+#    if bot.user != message.author:
+#        await bot.send_message(message.channel, "1145148101919")
 
 
 
