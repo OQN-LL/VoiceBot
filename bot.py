1946382b6e35ad6883ad48ecfd8e8c3f033c3074
OQN-LL <luta00000@gmail.com>
2018-09-08 02:15:09 +0900

    added file

---
+++ bot.py
@@ -0,0 +1,11 @@
+import discord
+
+class Bot:
+    def __init__(self,token,command_prefix="/"):
+        self.bot commands.Bot(command_prefix=commands.when_mentioned_or(command_prefix))
+        self.token = token
+
+    def run():
+        self.bot.run(self.token)
+
+    
--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 with open("accessToken.txt","r") as f:
     token = f.read()[:-1]
 
-bot = commands.Bot(command_prefix=commands.when_mentioned_or('/'))
+
 
 @bot.event
 async def on_ready():
@@ -13,19 +13,6 @@ async def on_ready():
     print("ID:",bot.user.id)
     print("-"*40)
 
-@commands.command(pass_context=True, no_pm=True)
-async def summon(ctx):
-    summoned_channel = ctx.message.author.voice_channel
-    if summoned_channel is None:
-        await bot.say('You are not in a voice channel.')
-        return False
-
-    state = get_voice_state(ctx.message.server)
-    if state.voice is None:
-        state.voice = await bot.join_voice_channel(summoned_channel)
-    else:
-        await state.voice.move_to(summoned_channel)
-    return True
 
 #@bot.event
 #async def on_message(message):
